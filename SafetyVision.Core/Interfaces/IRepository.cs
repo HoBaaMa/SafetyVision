eb01fb58d4b899020dd4ea3bcb7b00e6214a15c0
HoBaaMa <phacker935@gmail.com>
2025-10-11 02:40:13 +0300

    Refactor IRepository methods for entity identification
    - Changed GetByIdAsync to accept Guid instead of int.
    - Renamed DeleteAsync and UpdateAsync to Delete and Update.

--- SafetyVision.Core/Interfaces/IRepository.cs
+++ SafetyVision.Core/Interfaces/IRepository.cs
@@ -5,10 +5,10 @@ namespace SafetyVision.Core.Interfaces
     public interface IRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync();
-        Task<T?> GetByIdAsync(int id);
+        Task<T?> GetByIdAsync(Guid id);
         Task AddAsync(T entity);
-        void UpdateAsync(T entity);
-        void DeleteAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
         Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
     }
 }
--- SafetyVision.Infrastructure/Repositories/Repository.cs
+++ SafetyVision.Infrastructure/Repositories/Repository.cs
@@ -14,7 +14,7 @@ namespace SafetyVision.Infrastructure.Repositories
         }
         public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
 
-        public void DeleteAsync(T entity) => _context.Set<T>().Remove(entity); 
+        public void Delete(T entity) => _context.Set<T>().Remove(entity); 
 
         public Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
         {
@@ -23,9 +23,9 @@ namespace SafetyVision.Infrastructure.Repositories
 
         public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
 
-        public async Task<T?> GetByIdAsync(int id) => await _context.Set<T>().FindAsync(id);
+        public async Task<T?> GetByIdAsync(Guid id) => await _context.Set<T>().FindAsync(id);
 
 
-        public void UpdateAsync(T entity) => _context.Set<T>().Update(entity);
+        public void Update(T entity) => _context.Set<T>().Update(entity);
     }
 }
