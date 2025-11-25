b8e16371e75e8eb4f0bbed2fec1d4af9955f38c4
HoBaaMa <phacker935@gmail.com>
2025-11-25 21:20:30 +0200

    Add AutoMapper profile and update SafetyOfficerService
    Introduce a new mapping profile for `SafetyOfficer` and `SafetyOfficerDto` using AutoMapper. Update `SafetyOfficerService` to include methods for creating, deleting, retrieving, and updating safety officers. Implement checks for existing usernames and emails to prevent conflicts during creation and updates, ensuring data integrity.

---
+++ SafetyVision.Application/Mappings/SafetyOfficerMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using SafetyVision.Application.DTOs.SafetyOfficers;
+using SafetyVision.Core.Entities;
+
+namespace SafetyVision.Application.Mappings
+{
+    internal class SafetyOfficerMappingProfile : Profile
+    {
+        public SafetyOfficerMappingProfile()
+        {
+            CreateMap<SafetyOfficer, SafetyOfficerDto>().ReverseMap();
+            CreateMap<PostSafetyOfficerDto, SafetyOfficer>();
+        }
+    }
+}
---
+++ SafetyVision.Application/Services/SafetyOfficerService.cs
@@ -0,0 +1,116 @@
+﻿using AutoMapper;
+using SafetyVision.Application.DTOs.SafetyOfficers;
+using SafetyVision.Application.Interfaces;
+using SafetyVision.Core.Entities;
+using SafetyVision.Core.Enums;
+using SafetyVision.Core.Interfaces;
+using SafetyVision.Core.Utils;
+
+namespace SafetyVision.Application.Services
+{
+    public class SafetyOfficerService : ISafetyOfficerService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public SafetyOfficerService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task<Result<SafetyOfficerDto>> CreateAsync(PostSafetyOfficerDto dto)
+        {
+            if (await _unitOfWork.SafetyOfficers.GetByUserNameAsync(dto.Username) is not null)
+                return Result<SafetyOfficerDto>.Failure(ErrorType.Conflict, $"Username: {dto.Username} already exists.");
+
+            if (await _unitOfWork.SafetyOfficers.FindAsync(w => w.Email == dto.Email) is not null)
+                return Result<SafetyOfficerDto>.Failure(ErrorType.Conflict, $"Email: {dto.Email} already registered.");
+
+            var safetyOfficer = _mapper.Map<SafetyOfficer>(dto);
+
+            await _unitOfWork.SafetyOfficers.AddAsync(safetyOfficer);
+            await _unitOfWork.SaveChangesAsync();
+
+            return Result<SafetyOfficerDto>.Success(_mapper.Map<SafetyOfficerDto>(safetyOfficer));
+        }
+
+        public async Task<Result> DeleteAsync(Guid id)
+        {
+            var safetyOfficer = await _unitOfWork.SafetyOfficers.GetByIdAsync(id);
+            if (safetyOfficer is null) 
+                return Result.Failure(ErrorType.NotFound, $"Safety officer with ID: {id} not found.");
+
+            _unitOfWork.SafetyOfficers.Delete(safetyOfficer);
+            await _unitOfWork.SaveChangesAsync();
+
+            return Result.Success();
+        }
+
+        public async Task<Result<IEnumerable<SafetyOfficerDto>>> GetAllAsync()
+        {
+            var safetyOfficers = await _unitOfWork.SafetyOfficers.GetAllAsync();
+
+            return Result<IEnumerable<SafetyOfficerDto>>.Success(_mapper.Map<IEnumerable<SafetyOfficerDto>>(safetyOfficers));
+        }
+
+        public async Task<Result<IEnumerable<SafetyOfficerDto>>> GetAllByRoleTitleAsync(string roleTitle)
+        {
+            var safetyOfficers = await _unitOfWork.SafetyOfficers.GetAllAsync();
+            safetyOfficers = safetyOfficers.Where(r => r.RoleTitle == roleTitle);
+
+            return Result<IEnumerable<SafetyOfficerDto>>.Success(_mapper.Map<IEnumerable<SafetyOfficerDto>>(safetyOfficers));
+        }
+
+        public async Task<Result<SafetyOfficerDto>> GetByIdAsync(Guid id)
+        {
+            var safetyOfficer = await _unitOfWork.SafetyOfficers.GetByIdAsync(id);
+
+            if (safetyOfficer is null) 
+                return Result<SafetyOfficerDto>.Failure(ErrorType.NotFound, $"Safety officer with ID: {id} not found.");
+
+            return Result<SafetyOfficerDto>.Success(_mapper.Map<SafetyOfficerDto>(safetyOfficer));
+        }
+
+        public async Task<Result<SafetyOfficerDto>> GetByNameAsync(string name)
+        {
+            var safetyOfficer = await _unitOfWork.SafetyOfficers.GetByNameAsync(name);
+
+            if (safetyOfficer is null) 
+                return Result<SafetyOfficerDto>.Failure(ErrorType.NotFound, $"Safety officer with name: {name} not found.");
+
+            return Result<SafetyOfficerDto>.Success(_mapper.Map<SafetyOfficerDto>(safetyOfficer));
+        }
+
+        public async Task<Result<SafetyOfficerDto>> GetByUserNameAsync(string username)
+        {
+            var safetyOfficer = await _unitOfWork.SafetyOfficers.GetByUserNameAsync(username);
+
+            if (safetyOfficer is null)
+                return Result<SafetyOfficerDto>.Failure(ErrorType.NotFound, $"Safety officer with username: {username} not found.");
+
+            return Result<SafetyOfficerDto>.Success(_mapper.Map<SafetyOfficerDto>(safetyOfficer));
+        }
+
+
+        public async Task<Result> UpdateAsync(Guid id, PostSafetyOfficerDto dto)
+        {
+            var safetyOfficer = await _unitOfWork.SafetyOfficers.GetByIdAsync(id);
+
+            if (safetyOfficer is null)
+                return Result.Failure(ErrorType.NotFound, $"Safety officer with ID: {id} not found.");
+
+            if (await _unitOfWork.SafetyOfficers.GetByUserNameAsync(dto.Username) is not null && safetyOfficer.Id != id)
+                return Result<SafetyOfficerDto>.Failure(ErrorType.Conflict, $"Username {dto.Username} already exists.");
+
+            if (await _unitOfWork.SafetyOfficers.FindAsync(w => w.Email == dto.Email) is not null && safetyOfficer.Id != id)
+                return Result<SafetyOfficerDto>.Failure(ErrorType.Conflict, $"Email: {dto.Email} already registered.");
+
+            _mapper.Map(dto, safetyOfficer);
+
+            _unitOfWork.SafetyOfficers.Update(safetyOfficer);
+            await _unitOfWork.SaveChangesAsync();
+
+            return Result.Success();
+        }
+    }
+}
